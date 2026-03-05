de508b446033efe3cd8cbaea5ef375768317cec9
AlfreMoore <xmo@cs.wisc.edu>
2026-03-04 22:07:40 -0600

    ros2 init cheatsheet

--- init/ros2net_config.sh
+++ init/ros2net_config.sh
@@ -1,17 +1,19 @@
-# server
-fastdds discovery --server-id 0
-
-export ROS_DISCOVERY_SERVER=192.168.4.9:11811
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export ROS_DOMAIN_ID=22
-export ROS_LOCALHOST_ONLY=0
-
-# client
-export ROS_DISCOVERY_SERVER=192.168.4.9:11811
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export ROS_DOMAIN_ID=22
-export ROS_LOCALHOST_ONLY=0
-
-# Both
+# policy
 export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
 export ROS_STATIC_PEERS='192.168.4.4;remote.com'
+
+# fastdds discovery --server-id 0
+
+# export ROS_DISCOVERY_SERVER=192.168.4.9:11811
+# export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+# export ROS_DOMAIN_ID=22
+# export ROS_LOCALHOST_ONLY=0
+
+# driver
+export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+export ROS_STATIC_PEERS='192.168.4.4;remote.com'
+
+# export ROS_DISCOVERY_SERVER=192.168.4.9:11811
+# export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+# export ROS_DOMAIN_ID=22
+# export ROS_LOCALHOST_ONLY=0
